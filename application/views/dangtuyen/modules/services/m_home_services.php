fde11a5ef4cb54b719420b5fa22bb786479eedb1
BeTrai <betrai@BeTrais-MacBook-Pro.local>
2020-10-07 14:41:36 +0700

    update dịch vụ

--- application/views/dangtuyen/modules/services/m_home_services.php
+++ application/views/dangtuyen/modules/services/m_home_services.php
@@ -15,77 +15,27 @@
       </div>
     </div>	 <!-- END SECTION TITLE -->
   <?php endif;?>
+  <div class="row">
+    <?php
+    $i = 1;
+      foreach($items as $k=>$v):
+        $link = base_url()."dich-vu/".$v->slug->slug;
+    ?>
+      <div class='col-lg-6 col-md-6 mb-2 <?=($i%2==0)?'pl-4':'pr-4'?>'>
+        <div class='sbox mb-5'>
 
-    <div class="row">
-      <div class="col-md-12">
-        <!-- TABS NAVIGATION -->
-        <div id="tabs-nav" class="list-group text-center">
-            <ul class="nav nav-pills" id="pills-tab" role="tablist">
-              <?php
-              $active = "active";
-                foreach($items as $k => $v):
-              ?>
-              <!-- TAB-1 LINK -->
-              <li class="nav-item icon-xs">
-                <a class="nav-link <?=$active?>" id="tab<?=$v->id?>-list" data-toggle="pill" href="#tab-<?=$v->id?>" role="tab" aria-controls="tab-1" aria-selected="true">
-                  <!--<span class="flaticon-083-stethoscope"></span>--> <?=$v->translation->content->name?>
-                </a>
-              </li>
-              <?php $active = "";
-                endforeach;
-              ?>
-
-          </ul>
-
-        </div>	<!-- END TABS NAVIGATION -->
-
-
-        <!-- TABS CONTENT -->
-        <div class="tab-content" id="pills-tabContent">
-          <?php
-          $active = 'active';
-          foreach($items as $k=>$v):
-                $link = base_url()."dich-vu/".$v->slug->slug;
-            ?>
-          <!-- TAB-1 CONTENT -->
-          <div class="tab-pane fade show <?=$active?>" id="tab-<?=$v->id?>" role="tabpanel" aria-labelledby="tab<?=$v->id?>-list">
-            <div class="row d-flex align-items-center">
-
-              <?php
-                if(!empty($v->image)): ?>
-              <!-- TAB-1 IMAGE -->
-              <div class="col-lg-6">
-                <div class="tab-img">
-                  <?=img($v->image,'',array('class'=>'img-fluid','alt'=>$v->translation->content->name))?>
-                </div>
-              </div>
-            <?php endif;?>
-              <!-- TAB-1 TEXT -->
-              <div class="col-lg-6">
-                <div class="txt-block pc-30">
-
-                  <!-- Title -->
-                  <h3 class="h3-md green-color"><?=$v->translation->content->name?></h3>
-
-                  <?=$v->translation->content->content?>
-                  <!-- Button -->
-                  <a href="<?=$link?>" class="btn btn-orange orange-hover mt-30"><?=lang('View more detail')?></a>
-
-                </div>
-              </div>	<!-- END TAB-1 TEXT -->
-
-
-            </div>
-          </div>	<!-- END TAB-1 CONTENT -->
-
-        <?php
-            $active = "";
-            endforeach;?>
-        </div>	<!-- END TABS CONTENT -->
-
-
+        <?php if(!empty($v->image))
+              echo '<div class="tab-img">'.img($v->image,'',array('class'=>'img-fluid','alt'=>$v->translation->content->name)).'</div>';
+        ?>
+          <div class='service-title'  style='font-weight:bold;'>
+            <h4 class='h4-md h5-title text-uppercase'><?=anchor($link,$v->translation->content->name)?></h4>
+          </div>
+        </div>
       </div>
-    </div>     <!-- End row -->
-  </div>     <!-- End container -->
-</section>	<!-- END SERVICES-1 -->
+    <?php
+      $i++;
+    endforeach;
+    ?>
+  </div>
+</section>
 <?php endif;?>
