cd92a6a41b18e12b5c6916e46cf2d86684156c12
trantheanh82 <raijuichi@live.com>
2020-12-11 17:53:13 +0700

    Update m_home_services.php

--- application/views/dangtuyen/modules/services/m_home_services.php
+++ application/views/dangtuyen/modules/services/m_home_services.php
@@ -25,7 +25,7 @@
         <div class='sbox mb-5 box-shadow-dark'>
 
         <?php if(!empty($v->image))
-              echo '<div class="tab-img">'.anchor($link,img($v->image,'',array('class'=>'img-fluid','alt'=>$v->translation->content->name))).'</div>';
+              echo '<div class="tab-img text-center">'.anchor($link,img($v->image,'',array('class'=>'img-fluid','alt'=>$v->translation->content->name))).'</div>';
         ?>
           <div class='service-title text-center'  style='font-weight:bold;'>
             <h4 class='h4-md h5-title text-uppercase'><?=anchor($link,$v->translation->content->name)?></h4>
