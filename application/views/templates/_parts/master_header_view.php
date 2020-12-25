cec21df9b18ffcb2c5798859a59bd71540200dbb
trantheanh82 <raijuichi@live.com>
2020-12-26 00:13:26 +0700

    Update master_header_view.php

--- application/views/templates/_parts/master_header_view.php
+++ application/views/templates/_parts/master_header_view.php
@@ -23,6 +23,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="/<?=$Settings['company_favicon']?>">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <?php echo $before_head?>
 </head>
 
