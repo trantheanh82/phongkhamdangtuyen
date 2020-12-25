fd2e941b4df1a6cfb6c4a2f6dd439e272524eb11
trantheanh82 <raijuichi@live.com>
2020-12-25 23:22:56 +0700

    Update m_forms.php

--- application/views/dangtuyen/modules/m_forms.php
+++ application/views/dangtuyen/modules/m_forms.php
@@ -6,7 +6,7 @@
 				<div id="abox-3" class="col-md-4 col-lg-4  wow fadeInUp" data-toggle="modal" data-target="#bookingModal" data-modal-type="booking" data-modal-title="<?=lang("BOOKING APPOINTMENT ONLINE")?>" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
 					<div class="abox-1 green-color text-center">
 						<?=img('/assets/dangtuyen/images/icon-booking.png','',array('class'=>'img-fluid'))?>
-						<h4 class="h5-md mt-30">ĐẶT LỊCH KHÁM TIÊM CHỦNG ONLINE</h4>
+						<h5 class="h5-md mt-30">ĐẶT LỊCH KHÁM TIÊM CHỦNG ONLINE</h5>
 					</div>
 				</div>
 
@@ -15,7 +15,7 @@
 				<div id="abox-3" class="col-md-4 col-lg-4  wow fadeInUp"  data-wow-delay="0.6s" data-toggle="modal" data-modal-type="collection_blood" data-target="#bookingModal" data-modal-title="<?=lang("COLLECTION BLOOD")?>" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">
 					<div class="abox-1 green-color text-center">
 						<?=img('/assets/dangtuyen/images/icon-blood-test.png','',array('class'=>'img-fluid'))?>
-						<h4 class="h5-md mt-30">ĐĂNG KÝ LẤY MẪU XÉT NGHIỆM</h4>
+						<h5 class="h5-md mt-30">ĐĂNG KÝ LẤY MẪU XÉT NGHIỆM</h5>
 
 					</div>
 				</div>
@@ -24,7 +24,7 @@
 				<div id="abox-3" class="col-md-4 col-lg-4  wow fadeInUp" onclick="window.open('http://ykhoadangtuyen.com');"  data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInUp;">
 						<div class="abox-1 green-color text-center">
 							<?=img('/assets/dangtuyen/images/icon-search.png','',array('class'=>'img-fluid'))?>
-							<h4 class="h5-md mt-30">TRA CỨU KẾT QUẢ ONLINE</h4>
+							<h5 class="h5-md mt-30">TRA CỨU KẾT QUẢ ONLINE</h5>
 						</div>
 				</div>
 
