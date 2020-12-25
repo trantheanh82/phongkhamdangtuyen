4502da430b943ad19c1dd094fc375ae10664b781
trantheanh82 <raijuichi@live.com>
2020-12-26 01:20:15 +0700

    Update m_forms.php

--- application/views/dangtuyen/modules/m_forms.php
+++ application/views/dangtuyen/modules/m_forms.php
@@ -6,7 +6,7 @@
 				<div id="abox-3" class="col-md-4 col-lg-4  wow fadeInUp" data-toggle="modal" data-target="#bookingModal" data-modal-type="booking" data-modal-title="<?=lang("BOOKING APPOINTMENT ONLINE")?>" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
 					<div class="abox-1 green-color text-center">
 						<?=img('/assets/dangtuyen/images/icon-booking.png','',array('class'=>'img-fluid'))?>
-						<h5 class="h5-md mt-30">ĐẶT LỊCH KHÁM TIÊM CHỦNG ONLINE</h5>
+						<h5 class="h5-md mt-30">ĐẶT LỊCH KHÁM <br  />TIÊM CHỦNG ONLINE</h5>
 					</div>
 				</div>
 
