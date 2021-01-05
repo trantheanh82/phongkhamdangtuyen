47595db7f721698dd10a42d1054598b8463d62c3
trantheanh82 <raijuichi@live.com>
2021-01-05 15:25:51 +0700

    Update zalo_btn.php

--- application/views/dangtuyen/modules/zalo_btn.php
+++ application/views/dangtuyen/modules/zalo_btn.php
@@ -1,3 +1,16 @@
 <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
 
 <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+
+<style>
+.zalo-chat-widget{
+  z-index: 2147483647;
+    border: none;
+    visibility: visible;
+    bottom: 11%;
+    right: 4%;
+    position: fixed;
+    width: 70px;
+    height: 70px;
+}
+</style>
