d281611f5c494d2969de735a9d3bdb6df6d2ac56
trantheanh82 <raijuichi@live.com>
2021-01-05 15:18:53 +0700

    Update call_btn.php

--- application/views/dangtuyen/modules/call_btn.php
+++ application/views/dangtuyen/modules/call_btn.php
@@ -1,6 +1,6 @@
 <div style="position:absolute;">
   <a href="tel:<?=$Settings['company_phone_1']?>" class="call_now" rel="nofollow">
-  	<div class="mypage-alo-phone" style="bottom: 1%;">
+  	<div class="mypage-alo-phone" style="bottom: 0%;">
   		<div class="animated infinite zoomIn mypage-alo-ph-circle"></div>
   		<div class="animated infinite pulse mypage-alo-ph-circle-fill"></div>
   		<div class="animated infinite tada mypage-alo-ph-img-circle"></div>
