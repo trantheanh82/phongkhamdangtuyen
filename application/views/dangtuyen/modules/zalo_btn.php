21c5f84dcda41a5dd96e258063435a4a2c50d06f
trantheanh82 <raijuichi@live.com>
2021-01-06 14:21:32 +0700

    update zalo button

--- application/views/dangtuyen/modules/zalo_btn.php
+++ application/views/dangtuyen/modules/zalo_btn.php
@@ -1,3 +1,4 @@
 <div class="zalo-chat">
-	<a href="http://zalo.me/0918757775"><img src="https://stc-zaloprofile.zdn.vn/pc/v1/images/zalo_sharelogo.png" class="img-responsive" style="width:50px;"></a>
+	<a href="http://zalo.me/0918757775"><img src="https://stc-zaloprofile.zdn.vn/pc/v1/images/zalo_sharelogo.png" class="img-responsive" style="width:50px;">
+	<?=anchor('http://zalo.me/0918757775',img('/assets/images/zalo_sharelogo.png','',array('class'=>'img-responsive')))?></a>
 </div>
