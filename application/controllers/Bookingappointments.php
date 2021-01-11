73c2c3ecea7578a639df98da3c3f98a4a982b0a8
trantheanh82 <raijuichi@live.com>
2021-01-11 23:15:30 +0700

    Update Bookingappointments.php

--- application/controllers/Bookingappointments.php
+++ application/controllers/Bookingappointments.php
@@ -33,7 +33,14 @@ class Bookingappointments extends Public_Controller {
 				$this->email->to($this->data['Settings']['send_to']);
 				$this->email->cc('raijuichi@gmail.com');
 
-				$this->email->subject('Có Khách Đặt Lịch Khám');
+				$this->email->subject('Có Khách Đặt Lịch '.strtouppercase(lang($data['type']));
+
+				$msg = "Có đặt lịch ".strtouppercase(lang($data['type']))." khách hàng từ website<br />";
+				$msg. = "Thông tin khách hàng: <br />";
+				$msg. = "====================================<br /><br />";
+				$msg. = "- Tên khách hàng: ".$data['name'];
+				$msg. = "- Số điện thoại: ".$data['phone'];
+
 				$this->email->message("Có Đặt Lịch Khám mới");
 
 				if($this->email->send()){
